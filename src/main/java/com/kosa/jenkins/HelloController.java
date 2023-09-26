c707fd40d38a8216910462cf4f9d38649e460397
maverick <doncic1990@gmail.com>
2023-09-26 13:59:52 +0900

    commit

--- src/main/java/com/kosa/jenkins/HelloController.java
+++ src/main/java/com/kosa/jenkins/HelloController.java
@@ -1,17 +1,20 @@
 package com.kosa.jenkins;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 	
 	@GetMapping("/hello")
-	public String hello(Model model) {
-		model.addAttribute("message", "Hello Kosa!!!");
+	public ResponseEntity<Model> hello(Model model) {
 		
-		return "hello";
+		
+		return new ResponseEntity<Model> (model.addAttribute("message", "Hello Kosa!!!"), HttpStatus.OK);
 	}
 
 }
